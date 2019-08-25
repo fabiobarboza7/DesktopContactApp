83056c55ed3223f7ce45eae5024b173c2b7c5f69
Fábio Souza <fabiobarboza7@hotmail.com>
2019-08-25 20:26:43 -0300

    refactoring connection

--- NewContactWindow.xaml.cs
+++ NewContactWindow.xaml.cs
@@ -40,10 +40,11 @@ namespace DesktopContactsApp
             string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             string databasePath = System.IO.Path.Combine(folderPath, databaseName);
 
-            SQLiteConnection connection = new SQLiteConnection(databasePath);
-            connection.CreateTable<Contact>();
-            connection.Insert(contact);
-            connection.Close();
+            using (SQLiteConnection connection = new SQLiteConnection(databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+            }
 
             Close();
         }
