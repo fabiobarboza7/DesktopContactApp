2550a5ca3f10e7657d640b134266d87b8b1561e6
Fábio Souza <fabiobarboza7@hotmail.com>
2019-08-25 21:20:26 -0300

    read db

--- Classes/Contact.cs
+++ Classes/Contact.cs
@@ -10,7 +10,7 @@ namespace DesktopContactsApp.Classes
     public class Contact
     {
         [PrimaryKey, AutoIncrement]
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
